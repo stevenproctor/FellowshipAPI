57a9f7907cb8a32511d3611b20e689fd5cab26d7
stevenproctor <steven.proctor@gmail.com>
2011-07-07 20:19:56 -0500

    add in EditAddressRequest

---
+++ FellowshipLib/APIRequests/Get/EditAddressRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace FellowshipLib.APIRequests.Get
+{
+	public class EditAddressRequest : GetRequest<Address>
+	{
+		private const string PersonParameterId = "PersonId";
+		private const string AddressParameterId = "AddressId";
+		private const string Resource = "People/{" + PersonParameterId + "}/Addresses/{" + AddressParameterId + "}/edit";
+
+		private string personId;
+		private string addressId;
+
+		public EditAddressRequest(string personId, string addressId)
+		{
+			this.personId = personId;
+			this.addressId = addressId;
+		}
+
+		protected override void AddParameters(RestRequest request)
+		{
+			request.AddParameter(PersonParameterId, personId, ParameterType.UrlSegment);
+			request.AddParameter(AddressParameterId, addressId, ParameterType.UrlSegment);
+		}
+
+		protected override string GetResource()
+		{
+			return Resource;
+		}
+	}
+}
---
+++ FellowshipLib/Tests/EditAddressRequestTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FellowshipLib.APIRequests.Get;
+using FluentAssertions;
+
+namespace FellowshipLib.Tests
+{
+	[TestFixture]
+	public class EditAddressRequestTests
+	{
+		[TestCase]
+		[Category("IntegrationTest")]
+		public void WhenCallingEditAddressFor_AValidPerson_AndAValidAddressId_ItShouldReturnThatAddress()
+		{
+			var personId = "18970874";
+			var addressId = "12162767";
+			var editRequest = new EditAddressRequest(personId, addressId);
+			editRequest.Find();
+
+			editRequest.Succeeded().Should().BeTrue();
+			var result = editRequest.Result();
+			result.Should().NotBeNull();
+			result.Id.Should().Be(addressId);
+		}
+
+		[TestCase]
+		[Category("IntegrationTest")]
+		public void WhenCallingEditAddressFor_AnInvalidPerson_AndAValidAddressId_ItShouldReturnThatAddress()
+		{
+			var personId = "invalidPersonId";
+			var addressId = "12162767";
+			var editRequest = new EditAddressRequest(personId, addressId);
+			editRequest.Find();
+
+			editRequest.Succeeded().Should().BeTrue();
+			var result = editRequest.Result();
+			result.Should().NotBeNull();
+			result.Id.Should().Be(addressId);
+		}
+
+		[TestCase]
+		[Category("IntegrationTest")]
+		public void WhenCallingEditAddressFor_AnValidPerson_AndAnInvalidAddressId_ItShouldFail()
+		{
+			var personId = "18970874";
+			var addressId = "121627670000";
+			var editRequest = new EditAddressRequest(personId, addressId);
+			editRequest.Find();
+
+			editRequest.Succeeded().Should().BeFalse();
+			var result = editRequest.Result();
+			result.Should().BeNull();
+		}
+	}
+}
